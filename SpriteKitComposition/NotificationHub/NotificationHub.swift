d62a72555c18f8cc41b16f988f25748ad14b56a6
Seivan Heidari <seivan.heidari@icloud.com>
2014-12-04 03:54:24 +0100

    Set mocks to private.

--- SpriteKitComposition/NotificationHub/NotificationHub.swift
+++ SpriteKitComposition/NotificationHub/NotificationHub.swift
@@ -255,7 +255,7 @@ public func ==<T>(lhs: NotificationHub<T>, rhs: NotificationHub<T>) -> Bool { re
 #if DEBUG
   public struct NotificationHubMock {
     
-    static var onPublishMockHandler:((name:String, sender:AnyObject?, userInfo:Any?) -> (Void))?
+    private static var onPublishMockHandler:((name:String, sender:AnyObject?, userInfo:Any?) -> (Void))?
     static func onPublishingMockHandler(handler:(name:String, sender:AnyObject?, userInfo:Any?) -> (Void))  {
       self.onPublishMockHandler = handler
     }
